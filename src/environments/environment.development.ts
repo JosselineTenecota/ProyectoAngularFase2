07beeddb212a049b52493236bd6274fe3f9ffb81
JosselineTenecota <josselinetenecota@gmail.com>
2025-11-23 22:58:40 -0500

    Creacion de environment para firebase

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBpJT48gIXFmctMsZ3SeTQcCxfU0iPU7MA",
+    authDomain: "portafoliost-3cb34.firebaseapp.com",
+    projectId: "portafoliost-3cb34",
+    storageBucket: "portafoliost-3cb34.firebasestorage.app",
+    messagingSenderId: "219352220430",
+    appId: "1:219352220430:web:12777a60dd0ca962838d5f",
+    measurementId: "G-LGDDZSC0QV"
+  }
+};
