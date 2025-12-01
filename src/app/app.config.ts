87a6d9ac5e26cda2a7bd90db632a19dc3130dd32
ErickSalinas30 <erick27salinas@gmail.com>
2025-12-01 08:57:13 -0500

    cambios de app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,33 +1,20 @@
-import { 
-  ApplicationConfig, 
-  provideBrowserGlobalErrorListeners, 
-  provideZoneChangeDetection 
-} from '@angular/core';
-
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDuaZ0u_E3PHJ2gfP7Qp55_-IeaBRrKXqA",
-  authDomain: "salitenecoportafolio.firebaseapp.com",
-  projectId: "salitenecoportafolio",
-  storageBucket: "salitenecoportafolio.firebasestorage.app",
-  messagingSenderId: "110341634338",
-  appId: "1:110341634338:web:f0aebb21a1f361f8b90d7b"
-};
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ]
 };
